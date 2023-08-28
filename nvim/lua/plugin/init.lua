1d228b9d5f13d9d3875c9d7c6191bb16389a8b1a
高浩 <gaohao.wherever@bytedance.com>
2023-08-28 11:22:57 +0800

    gpt

--- nvim/lua/plugin/init.lua
+++ nvim/lua/plugin/init.lua
@@ -198,7 +198,25 @@ require('lazy').setup({
     {
         'dstein64/vim-startuptime',
         version = 'v4.4.0'
+    },
+
+    {
+        'MunifTanjim/nui.nvim',
+        version = 'v0.2.0'
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("chatgpt").setup()
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
     }
+
 }
 )
 
