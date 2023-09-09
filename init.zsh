0b8d5ec01633ad67fe0bad5342f2950090bac99e
wherever230 <wherever230@gmail.com>
2023-09-09 16:54:31 +0800

    modify zsh arch

---
+++ init.zsh
@@ -0,0 +1,14 @@
+source $CODEPATH/git/oh-my-env/zsh/install
+
+# plugins config
+plugins=(git zsh-autosuggestions kubectl vi-mode kubectx golang ag history zsh-syntax-highlighting)
+ZSH_THEME="spaceship"
+zstyle ':omz:update' mode auto      # update automatically without asking
+
+source $ZSH/oh-my-zsh.sh
+source $CODEPATH/git/oh-my-env/zsh/export
+source $CODEPATH/git/oh-my-env/zsh/alias
+source $CODEPATH/git/oh-my-env/zsh/bindkey
+source $CODEPATH/git/oh-my-env/zsh/function
+
+# vim: set filetype=bash :
