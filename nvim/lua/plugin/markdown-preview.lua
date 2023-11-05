34c3fac7a1df4a13c24ac26ba28da1226c244b28
高浩 <gaohao.wherever@bytedance.com>
2023-11-05 11:56:47 +0800

    fix

--- nvim/lua/plugin/markdown-preview.lua
+++ nvim/lua/plugin/markdown-preview.lua
@@ -5,3 +5,8 @@ keyset("n", "<Leader>mds", ":MarkdownPreview<CR>")
 local g = vim.g
 g.mkdp_port = "9090"
 g.mkdp_theme = "dark"
+g.mkdp_browser = "edge"
+
+if vim.loop.os_uname().sysname == "Darwin" then
+    g.mkdp_browser = "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"
+end
