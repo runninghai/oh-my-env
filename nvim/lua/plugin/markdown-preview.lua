0eca526123cd3b6b3fbb0a2c1abb5ac1c462f43f
wherever230 <wherever230@gmail.com>
2023-11-06 09:16:16 +0800

    fix tool

--- nvim/lua/plugin/markdown-preview.lua
+++ nvim/lua/plugin/markdown-preview.lua
@@ -5,8 +5,8 @@ keyset("n", "<Leader>mds", ":MarkdownPreview<CR>")
 local g = vim.g
 g.mkdp_port = "9090"
 g.mkdp_theme = "dark"
-g.mkdp_browser = "edge"
+g.mkdp_browser = "microsoft-edge"
 
 if vim.loop.os_uname().sysname == "Darwin" then
-    g.mkdp_browser = "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"
+  g.mkdp_browser = "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"
 end
